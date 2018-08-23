15b8f14f88dda5cf4b47a42df52393221f904b39
alison smith <smithpyz@gmail.com>
2018-08-23 08:24:45 -0400

    fixing routes

--- serverAA.js
+++ serverAA.js
@@ -30,8 +30,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app,express);
 
 // =============================================================================
 // LISTENER
